5557ef9ae9364561876dcb70658397ec65a06a15
Krishna Nannuru <knannuru@nvidia.com>
2026-07-11 21:41:27 -0700

    i

--- e.mk
+++ e.mk
@@ -8,12 +8,15 @@ UBUNTU_INSTALLER := ./a/os/linux/ubuntu/install.sh
 FEDORA_INSTALLER := ./a/os/linux/fedora/install.sh
 PLATFORM_INSTALLER := ./a/os/install.sh
 
-.PHONY: all Darwin Linux kws deps ldeps python-deps tree-sitter-src syncthing \
+.PHONY: all install Darwin Linux kws deps ldeps python-deps tree-sitter-src syncthing \
 	configure build codesign vterm-module install-a site-lisp-sync userdir \
 	local-bin brew-bin launch system brew-check brew-install help
 
 all:
 	$(PLATFORM_INSTALLER) all
+
+install:
+	$(PLATFORM_INSTALLER) install
 
 Darwin:
 	$(DARWIN_INSTALLER) all
